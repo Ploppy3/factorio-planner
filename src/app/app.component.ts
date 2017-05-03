4bba79be50f0b47fbdc8fae352a5c0e6131742e8
Robin Noyel <greenplop@gmail.com>
2017-05-03 23:19:33 +0200

    add version number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { DataService } from "./data.service";
 
+declare var require: any;
+const { version } = require('../../package.json');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +12,8 @@ import { DataService } from "./data.service";
   providers: [DataService]
 })
 export class AppComponent implements OnInit {
+
+  public version: string = version;
 
   private readonly OPEN_BRACE: number = 0;
   private readonly CLOSE_BRACE: number = 1;
