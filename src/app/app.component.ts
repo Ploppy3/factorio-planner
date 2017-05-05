b88fabaf9c07121dbefb4f3a70335c06fe3a5c28
Robin Noyel <greenplop@gmail.com>
2017-05-05 16:20:54 +0200

    fix method visibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
   }
 
-  private reloadData() {
+  public reloadData() {
     this.dataService.loadData(this.dataVersionId);
     this.outputNode.reload();
   }
