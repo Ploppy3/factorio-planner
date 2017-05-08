1fd041cd7d274e44a9352941055e388ffe04a6bd
Robin Noyel <greenplop@gmail.com>
2017-05-08 19:59:54 +0200

    code refactoring and perf improvements

--- src/app/node.ts
+++ src/app/node.ts
@@ -3,9 +3,10 @@ import { PlannerService } from "./planner.service";
 
 export class Node {  
 
-  //for test purpose
-  public outputRate: number = 1;
+  private recipe: any; //game recipe provided by the data service
 
+  private useExpensive: boolean = false;
+  public outputRate: number = 1;
   public name: string = 'Item';
   public recipeRequest: number = 1;
   public quantityPerCraft: number = 1;
@@ -23,11 +24,12 @@ export class Node {
 
   constructor(private dataService: DataService, private plannerService: PlannerService){}
 
-  public calculate() {
+  public calculate() { // small possible refresh
+    //console.log('calculate', this.name);
     //this.attenuation = (this.recipeRequest / this.quantityPerCraft) * this.parent.attenuation;
     //this.numberMachines = this.craftingTime * (this.recipeRequest * this.parent.attenuation) / this.craftingSpeed / this.quantityPerCraft;
     this.outputRate = this.parent.outputRate / this.parent.quantityPerCraft * this.recipeRequest;
-    this.numberMachines = this.outputRate / ((1 * this.craftingSpeed) / this.craftingTime);
+    this.numberMachines = this.outputRate / ((1 * this.craftingSpeed) /  this.craftingTime);
     //this.rate = this.quantityPerCraft / this.craftingTime * this.craftingSpeed * this.numberMachines;
     //this.rate = 0;
     this.childs.forEach(node => {
@@ -35,68 +37,86 @@ export class Node {
     });
   }
 
-  public findRecipeByName() {
+  public refreshExpensiveRecipes(useExpensive: boolean) {
+    this.useExpensive = useExpensive;
+    this.childs.forEach(node => {
+      node.refreshExpensiveRecipes(useExpensive);
+    });
+  }
+
+  public initialize() {
+    if (!this.recipe) { return; }
+    //console.log(recipe);
+    let ingredients: any[] = [];
+    //-------------------------------
+    if (this.recipe.category) {
+      this.category = this.recipe.category;
+    } else {
+      this.category = 'crafting'; //default
+    }
+    this.quantityPerCraft = 1; // reset value (needed for root node only)
+    let craftingTime_expensive: number = null;
+    if (this.recipe.expensive && this.plannerService.useExpensiveRecipes$.value) { // expensive
+      if (this.recipe.normal.ingredients) ingredients = this.recipe.expensive.ingredients;
+      if (this.recipe.normal.energy_required) craftingTime_expensive = this.recipe.expensive.energy_required;
+      //console.log('using expensive recipe', this.recipe.name);
+    } else if (this.recipe.normal) { // normal
+      if (this.recipe.normal.ingredients) ingredients = this.recipe.normal.ingredients;
+      if (this.recipe.normal.energy_required) this.craftingTime = this.recipe.normal.energy_required;
+    } else { // basic
+      if (this.recipe.ingredients) ingredients = this.recipe.ingredients;
+      if (this.recipe.energy_required) this.craftingTime = this.recipe.energy_required;
+      if (this.recipe.result_count) this.quantityPerCraft = this.recipe.result_count;
+    }
+    if (this.recipe.results) {
+      this.recipe.results.forEach(result => {
+        if (result.name == this.recipe.name) {
+          this.quantityPerCraft = result.amount;
+        }
+      });
+    }
+    let machines = this.dataService.getAssemblingMachinesByCategory(this.category, ingredients.length);
+    this.machines = machines;
+    if (machines.length > 0) {
+      this.craftingSpeed = machines[0].crafting_speed;
+      //console.log('using machine:', machines[0], 'for', recipe.name);
+    } else {
+      this.craftingSpeed = 0;
+    }
+    //-------------------------------
+    this.calculate(); //do it here to prevent propagation of the calculate call to its children
+    //-------------------------------
+    ingredients.forEach(ingredient => {
+      if (Array.isArray(ingredient)) { // type 1 - array
+        this.addChild(ingredient[0], ingredient[1]);  // name, amount
+      } else { // type 2 - object
+        this.addChild(ingredient.name, ingredient.amount);
+      }
+    });
+  }
+
+  public findRecipeByName() { // starts a major refresh
+    //console.log('searching recipe', this.name);
     this.childs = [];
     for (let i = 0; i < this.dataService.recipes.length; i++) {
       let recipe = this.dataService.recipes[i];
       if (recipe.name == this.name) {
-        console.log(recipe);
-        let ingredients: any[] = [];
-        //-------------------------------
-        if (recipe.category) {
-          this.category = recipe.category;
-        } else {
-          this.category = 'crafting'; //default
-        }
-        this.quantityPerCraft = 1; // reset value (needed for root node only)
-        if (recipe.expensive && this.plannerService.useExpensiveRecipes$.value) { // expensive
-          if (recipe.normal.ingredients) ingredients = recipe.expensive.ingredients;
-          if (recipe.normal.energy_required) this.craftingTime = recipe.expensive.energy_required;
-          //console.log('using expensive recipe for', recipe)
-        } else if (recipe.normal) { // normal
-          if (recipe.normal.ingredients) ingredients = recipe.normal.ingredients;
-          if (recipe.normal.energy_required) this.craftingTime = recipe.normal.energy_required;
-        } else { // basic
-          if (recipe.ingredients) ingredients = recipe.ingredients;
-          if (recipe.energy_required) this.craftingTime = recipe.energy_required;
-          if (recipe.result_count) this.quantityPerCraft = recipe.result_count;
-        }
-        if (recipe.results) {
-          recipe.results.forEach(result => {
-            if (result.name == recipe.name) {
-              this.quantityPerCraft = result.amount;
-            }
-          });
-        }
-        let machines = this.dataService.getAssemblingMachinesByCategory(this.category, ingredients.length);
-        if (machines.length > 0) {
-          this.machines = machines;
-          this.craftingSpeed = machines[0].crafting_speed;
-          //console.log('using machine:', machines[0], 'for', recipe.name);
-        }
-        //-------------------------------
-        this.calculate(); // do it at this moment to prevent calculate call to propagate to its childs
-        //-------------------------------
-        ingredients.forEach(ingredient => {
-          if (Array.isArray(ingredient)) { // type 1 - array
-            this.addChild(ingredient[0], ingredient[1]);  // name, amount
-          } else { // type 2 - object
-            this.addChild(ingredient.name, ingredient.amount);
-          }
-        });
-        return recipe;
+        this.recipe = recipe;
+        //console.log(recipe);
+        this.initialize();
+        return;
       }
     }
-    this.category = 'unknown';
-    console.log("can't find recipe for", this.name);
+    this.category = 'unknown'; // reset for root node
+    //console.log("can't find recipe for", this.name);
   }
 
-  public addChild(name?: string, output?: number, category?: string) {
+  public addChild(name: string, recipeRequest: number) {
     let node = new Node(this.dataService, this.plannerService);
     node.parent = this;
-    if (name) node.name = name
-    if (output) node.recipeRequest = output;
-    node.calculate();
+    node.name = name
+    node.recipeRequest = recipeRequest;
+    //node.calculate();
     this.childs.push(node);
   }
 }
