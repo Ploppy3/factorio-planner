7e4843cc5cd30eda1ca7533eb1caaa9df9fb8ee4
Robin Noyel <greenplop@gmail.com>
2017-05-08 13:00:05 +0200

    return empty array instead of full array when no input text

--- src/app/url-img.pipe.ts
+++ src/app/url-img.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class UrlImgPipe implements PipeTransform {
 
-  transform(value: string, args?: any): any {
+  transform(value: string, args?: any): any { // set first letter of 'value' as a capital letter
     return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
   }
 
