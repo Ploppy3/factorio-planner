d1c4b372ad10a53a8c529fc2b2fcf269dd8bd259
Robin Noyel <greenplop@gmail.com>
2018-01-22 00:31:46 +0100

    feat(dialog.comp) changelog defined in .ts rather than in html

--- src/app/dialog-changelog/dialog-changelog.component.ts
+++ src/app/dialog-changelog/dialog-changelog.component.ts
@@ -7,9 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogChangelogComponent implements OnInit {
 
+  public versions: Version[] = [
+    {
+      name: 'Version 1.5.0',
+      changes: [
+        'Fixed broken images',
+      ]
+    },
+    {
+      name: 'Version 1.4.0',
+      changes: [
+        'Added Resource Overview',
+        'Improved performance',
+      ]
+    },
+    {
+      name: 'Version 1.3.0',
+      changes: [
+        'Added global settings for crafting machines',
+        'Improved performance',
+      ]
+    },
+    {
+      name: 'Version 1.2.0',
+      changes: [
+        'Added new highly requested Tree View',
+        'Added ability to minimize nodes by clicking on their icons',
+        'Added new time units',
+      ]
+    },
+    {
+      name: 'Version 1.1.0',
+      changes: [
+        'Added expensive recipes (marathon)',
+      ]
+    },
+    {
+      name: 'Version 1.0.0',
+      changes: [
+        'Initial release test',
+      ]
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+interface Version {
+  name: string;
+  changes: string[];
 }
