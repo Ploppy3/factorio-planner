a0b9e6c811b252e9f9dc4e96314c70a2a3713d7f
Robin Noyel <greenplop@gmail.com>
2018-01-22 01:56:52 +0100

    feat(nodes.comp) add new animation

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -13,15 +13,14 @@ export const fadeInOut = trigger('fadeInOut', [
 ]);
 
 export const reveal = trigger('reveal', [
-  transition(':enter', animate('500ms ease-out', keyframes([
-    style({ opacity: 0, height: 0, width: 0, padding: 0, offset: 0 }),
-    style({ opacity: 0, height: '*', width: '*', padding: '*', offset: .5 }),
-    style({ opacity: 1, offset: 1 }),
-  ]))),
-  transition(':leave', animate('500ms ease-out', keyframes([
-    style({ opacity: 0, offset: .5 }),
-    style({ height: 0, width: 0, padding: 0, offset: 1 }),
-  ]))),
+  transition(':enter', [
+    style({ paddingTop: 0, paddingBottom: 0, height: 0, overflow: 'hidden' }),
+    animate('500ms ease-out', style({ paddingTop: '*', paddingBottom: '*', height: '*' }))
+  ]),
+  transition(':leave', [
+    style({ overflow: 'hidden' }),
+    animate('500ms ease-out', style({ paddingTop: 0, paddingBottom: 0, height: 0, overflow: 'hidden' }))
+  ]),
 ]);
 
 export const fadeInOut2 = trigger('fadeInOut', [
