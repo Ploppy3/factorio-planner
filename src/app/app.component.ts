9cac8b71140a021d010d439488d551b63816200e
Robin Noyel <greenplop@gmail.com>
2018-01-23 15:27:07 +0100

    feat(app.comp) add possibility to hide tips

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/core';
 import { MatDialog } from "@angular/material";
 
 import { VirtualOutputNodeComponent } from "./virtual-output-node/virtual-output-node.component";
@@ -13,12 +14,29 @@ const { version } = require('../../package.json');
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [DataService, PlannerService]
+  providers: [DataService, PlannerService],
+  animations: [
+    trigger('tips',[
+      state('hidden', style({
+        display: 'none',
+      })),
+      transition('visible => hidden', [
+        animate('.5s ease-in-out', style({
+          height: 0,
+          margin: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          opacity: 0,
+        }))
+      ])
+    ])
+  ],
 })
 export class AppComponent implements OnInit {
 
   @ViewChild('outputNode') outputNode: VirtualOutputNodeComponent;
 
+  public showTips = true;
   public appVersion: string = version;
   public dataVersion: string = this.dataService.dataVersions[0].name;
 
