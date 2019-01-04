ff5d4646cadd42ee79b2a86848a9b6de406e06b6
Robin Noyel <greenplop@gmail.com>
2019-01-04 14:53:06 +0100

    feat(dialog-support.comp) animate image reveal

--- src/app/dialog-support/dialog-support.component.ts
+++ src/app/dialog-support/dialog-support.component.ts
@@ -1,11 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, state, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-dialog-support',
   templateUrl: './dialog-support.component.html',
-  styleUrls: ['./dialog-support.component.scss']
+  styleUrls: ['./dialog-support.component.scss'],
+  animations: [
+    trigger('revealImage', [
+      state('notLoaded', style({
+        height: 0,
+        opacity: 0,
+      })),
+      state('loaded', style({
+        height: '*',
+        opacity: 1,
+      })),
+      transition('notLoaded -> loaded', [
+        style({
+          height: 0,
+          opacity: 0,
+        }),
+        animate('.25s 0s ease', style({
+          height: '*',
+        })),
+        animate('.25s 0s ease', style({
+          opacity: 1,
+        })),
+      ]),
+    ]),
+  ]
 })
 export class DialogSupportComponent implements OnInit {
+
+  public imageLoaded = false;
 
   constructor() { }
 
