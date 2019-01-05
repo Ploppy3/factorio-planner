3df0b6ed8c8f2acf5a075a280f7755942f9f3429
Robin Noyel <greenplop@gmail.com>
2019-01-05 14:45:38 +0100

    fix(mat.module) remove  icon module import

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   MatButtonModule,
-  MatIconModule,
   MatAutocompleteModule,
   MatInputModule,
   MatSelectModule,
@@ -21,7 +20,6 @@ import {
   ],
   exports: [
     MatButtonModule,
-    MatIconModule,
     MatAutocompleteModule,
     MatInputModule,
     MatSelectModule,
